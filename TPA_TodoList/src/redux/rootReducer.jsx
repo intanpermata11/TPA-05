d301e54414b26f3e1d69c0c503a1ec6e6b46be26
Intan Permatasari <83773598+intanpermata11@users.noreply.github.com>
2022-11-15 00:21:05 +0700

    Delete TPA_TodoList directory

--- TPA_TodoList/src/redux/rootReducer.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { operationsReducer } from "./todoapp/reducers/operations";
-
-export const rootReducer = combineReducers({
-    operationsReducer,
-    // more reducers can be added here
-})
